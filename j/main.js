45f6be0c0647265f81a6744c759eef7739ab8f1b
Justin <jlaulkl@gmail.com>
2015-05-12 21:08:37 +0800

    empty field after submit

--- j/main.js
+++ j/main.js
@@ -46,6 +46,7 @@ app.directive('socialmedia', function () {
 
 app.controller('fpCtrl', function($scope, $http) {
   $scope.formData = '';
+  
   $scope.processForm = function() {
     $http({
       method  : 'POST',
@@ -53,15 +54,13 @@ app.controller('fpCtrl', function($scope, $http) {
       data    : $.param($scope.formData),  // pass in data as strings
       headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)
       })
-    .success(function(data) {
-      console.log(data);
-        if (data.success) {
-          // if not successful, bind errors to error variables
-          // $scope.errorEmail = data.errors.email;
-
+    .success(function(data, status, headers, config) {
+      
+        if (status == '200') {
+          // if successful, set field to empty
+          $scope.formData = null;
         } else {
-          // if successful, bind success message to message
-          //$scope.message = data.message;
+          
         }
     });
   };
