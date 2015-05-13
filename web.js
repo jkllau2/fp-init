09a25b5ee78e3378afa7220d9e7539426f319355
Justin <jlaulkl@gmail.com>
2015-05-13 15:33:44 +0800

    web.js

--- web.js
+++ web.js
@@ -1,7 +1,10 @@
-var gzippo = require('gzippo');
 var express = require('express');
 var app = express();
- 
-app.use(express.logger('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 5000);
+
+app.use(express.static(__dirname + '/foodpose'));
+
+app.get('/', function(req, res){
+  res.redirect('/index.html');
+});
+
+app.listen(80);
